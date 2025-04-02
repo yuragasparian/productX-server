3b64c04488986209cb743c5731fecd52e4cb9d46
Yura Gasparyan <yuragaparyan@gmail.com>
2025-04-02 13:14:29 +0400

    Initialized Express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-console.log("Starting ...");
-console.log("Starting ...");
+import express from "express"
+import cors from "cors"
+import http from "http"
+import compression from "compression"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+
+
+const app = express()
+app.use(cors(
+    {
+        credentials: true
+    }
+))
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(process.env.PORT, () => {
+    console.log("Server is running on", process.env.APP_URL);
+})
