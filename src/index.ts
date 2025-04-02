0df806e230a19ee73cdb3c01765a8a68a1834139
Yura Gasparyan <yuragaparyan@gmail.com>
2025-04-02 20:26:18 +0400

    Modified Prisma schema

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,6 @@ import http from "http"
 import compression from "compression"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
-import addProduct from './test.prisma';
-
-
 
 const app = express()
 app.use(cors(
@@ -22,8 +19,6 @@ const server = http.createServer(app)
 
 server.listen(process.env.PORT, () => {
     console.log("Server is running on", process.env.APP_URL);
-    
 })
 
-addProduct()
 
